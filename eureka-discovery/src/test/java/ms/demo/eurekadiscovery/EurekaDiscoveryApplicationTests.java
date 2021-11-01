6703897e1f71f8b7c9ee195612fefaa59e63b11a
Ayoub <ayoubmouallim@gmail.com>
2021-11-01 12:26:12 +0100

    dev: eureka discovery

---
+++ eureka-discovery/src/test/java/ms/demo/eurekadiscovery/EurekaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.demo.eurekadiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
