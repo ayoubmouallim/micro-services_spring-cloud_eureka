a1f03cbe9f7ca6f7137955388bed372ebe01115a
Ayoub <ayoubmouallim@gmail.com>
2021-11-01 12:25:36 +0100

    dev: spring cloud gateaway

---
+++ gateway/src/main/java/ms/demo/gateway/GatewayApplication.java
@@ -0,0 +1,36 @@
+package ms.demo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+   // @Bean
+    /*RouteLocator routeLocator(RouteLocatorBuilder builder)
+    {
+
+      return builder.routes()
+          .route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+          .route(r->r.path("/products/**").uri("lb://INVENTORY-SERVICE")).build();
+    }*/
+
+    // dynamic routes
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp)
+    {
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    }
+
+}
